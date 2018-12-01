c386b424330c99a6983f01964461969b53086fa6
LinuxPlus <>
2018-12-01 17:15:23 +0300

    L3

---
+++ Concetration-4-L1/Concentration.swift
@@ -0,0 +1,63 @@
+//
+//  Concetration.swift
+//  Concetration-4-L1
+//
+//  Created by LinuxPlus on 11/17/18.
+//  Copyright © 2018 Stanford Uneversity. All rights reserved.
+//
+
+import Foundation
+
+// class is passed as reference
+class Concentration
+{
+	private(set) var cards = [Card]()
+	
+	private var indexOfOneAndOnlyFaceUpCard: Int? {
+		get {
+			var foundIndex: Int?
+			for index in cards.indices {
+				if cards[index].isFaceUp {
+					if foundIndex == nil {
+						foundIndex = index
+					} else {
+						return nil
+					}
+				}
+			}
+			return foundIndex
+		}
+		set {
+			for index in cards.indices {
+				cards[index].isFaceUp = (index == newValue)
+			}
+		}
+	}
+	
+	func chooseCard(at index: Int) {
+		assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): shosen index not in the cards")
+		if !cards[index].isMatched {
+			if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+//				check if cards match
+				if cards[matchIndex].identifier == cards[index].identifier {
+					cards[matchIndex].isMatched = true
+					cards[index].isMatched = true
+				}
+				cards[index].isFaceUp = true
+			} else {
+				indexOfOneAndOnlyFaceUpCard = index
+			}
+		}
+	}
+	
+	init(numberOfPairsOfCards: Int) {
+		assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): you must have at least one pair of cards")
+		for _ in 0..<numberOfPairsOfCards {
+			let card = Card()
+			self.cards += [card, card]
+			// TODO: Shiffle the cards
+		}
+	}
+}
+
+
